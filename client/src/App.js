1e09cab7c4ab878c46f21e1a4954b66e7f358e1d
Bryan DenHollander <bryan.denhollander@gmail.com>
2018-10-11 15:16:19 -0400

    added new UI

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,9 @@ class App extends Component {
   
   getTweets = () => {
 	  //Get the tweets and store them in the state
-	  fetch('/api/tweets')
-		.then(res => res.json())
-		.then(tweets => this.setState({ tweets }));
+//	  fetch('/api/tweets')
+//		.then(res => res.json())
+//		.then(tweets => this.setState({ tweets }));
   }
   
   componentDidMount(){
@@ -21,20 +21,57 @@ class App extends Component {
 	const { tweets } = this.state;
 	
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+		<div className="App">
+			<h1>MLH Sample App</h1>
+			<div className="container">
+			  <ul className="dashboard-list">
+				<li key="1">
+				  <div className='tweet'>
+					<div className='tweet-info'>
+					  <div>
+						<span>Major League Hacking</span>
+						<div>10/10/17</div>
+						<p>This is what we were tweeting about on this date!</p>
+					  </div>    
+					  <div className='tweet-icons'>
+						<span>Replys</span>
+						<span>Likes</span>
+					  </div>
+					</div>
+				  </div>
+				</li>
+				<li key="2">
+				  <div className='tweet'>
+					<div className='tweet-info'>
+					  <div>
+						<span>Major League Hacking</span>
+						<div>10/10/17</div>
+						<p>This is what we were tweeting about on this date!</p>
+					  </div>    
+					  <div className='tweet-icons'>
+						<span>Replys</span>
+						<span>Likes</span>
+					  </div>
+					</div>
+				  </div>
+				</li>
+				<li key="3">
+				  <div className='tweet'>
+					<div className='tweet-info'>
+					  <div>
+						<span>Major League Hacking</span>
+						<div>10/10/17</div>
+						<p>This is what we were tweeting about on this date!</p>
+					  </div>    
+					  <div className='tweet-icons'>
+						<span>Replys</span>
+						<span>Likes</span>
+					  </div>
+					</div>
+				  </div>
+				</li>
+			  </ul>
+			</div>
       </div>
     );
   }
