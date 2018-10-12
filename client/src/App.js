0ce34d9074f0040d922924897445453b71654128
Bryan DenHollander <bryan.denhollander@gmail.com>
2018-10-12 12:16:27 -0400

    added comments and cleaned up code

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,63 @@
-import React, { Component } from 'react';
-import './App.css';
-import Tweet from './components/Tweet'
-import axios from 'axios';
+import React, { Component } from 'react'; 
+import './App.css';  /* Import our styling. This can be modified as you see fit */
+import Tweet from './components/Tweet' /* Import our tweet component from the components folder */
+import axios from 'axios'; /* Import axios. We will use this to make our http requests to our backend */
 
 class App extends Component {
-  //Initialize the state
+  /* 
+  	We will initialize the state to an empty array.
+	Later, we will populate this array with our tweets.
+  */
   state = {statuses:[]}
   
   
+  /*
+	componentDidMount()
+		This function is executed when the page has loaded and is ready for data.
+		Your calls to your backend should be in this function.
+  */
   componentDidMount(){
-	  axios.get('https://salty-ridge-74365.herokuapp.com/api/tweets')
+
+  	/*
+		We are using axios to send the GET request to our backend.
+		For development, our backend API endpoint is at
+			http://localhost:5000/api/tweets.
+		For production, our backend API endpoint is at
+			https://salty-ridge-74365.herokuapp.com/api/tweets
+		When we push the app into proudction, we need to remember to switch
+		the get request from "http://localhost5000 ... " to 
+		"https://salty-ridge-743 ..."  If we don't make the switch, the client
+		will make its API call to the wrong place!
+  	*/
+	  //axios.get('http://localhost:5000/api/tweets') /*This is for development */
+	  axios.get('https://salty-ridge-74365.herokuapp.com/api/tweets') /* This is for production */
       .then(response => {
       	let statuses = response.data.statuses
       	this.setState({statuses})
-      	console.log(statuses)
       })
   }
   
+  /* The render function is what makes our UI */
   render() {
-
+  	/*Put any logic, parsing, or debugging output here */
   	let tweets = this.state.statuses;
-
 	console.log(tweets);
-	
     return (
+    	/*And put your UI that you'd like to display, here */
 		<div className="App">
 			<h1>MLH Sample App</h1>
 			<div className="container">
 			  <ul className="dashboard-list">
-				{tweets.map(index => (
+				{tweets.map(index => ( /* 
+											We are just iterating over the array of 
+											tweets and creating a Tweet component for each item in the array
+										*/
 					<li key={index.id}>
 						<Tweet author={index.user.name} date={index.created_at} text={index.text} pic={index.user.profile_image_url}/>
-					</li>
+					</li> 	/* 
+								This is our tweet component. Its responsible for displaying the data we pass to it.
+								To see more, check out ./components/Tweet.js 
+							*/
 				))}	
 			  </ul>
 			</div>
--- index.js
+++ index.js
@@ -1,83 +1,135 @@
-const express = require('express')
-const path = require('path')
-const twit = require('twit')
-var config = require('./config.js')
+/*
+	The backend server operates in a very simple manner.
+	Every 5 minutes, it makes a request to the the Twitter API for the
+	latest Tweets. 
+
+	When the latest tweets are recieved, the server updates its local
+	tweets variable with the tweets recieved. 
+
+	This is called polling. 
+
+	Each time our frontend is loaded in the browser, it makes a request
+	to the backend for the local tweets variable. When the frontend recieves
+	the tweets, it displays them. 
+
+	[Our Server] --- Every 5 minutes ----> [Twitter API]
+
+	[Our Frontend] ---- When it loads -----> [Our Server]
+
+*/
 
 
-//initialize our Twitter object with credentials we specified in Config.js
-const Twitter = new twit(config);
+const express = require('express')  /* This is the framework for our backend server */
+const path = require('path')	/* This is a module the provides an easy way to work with files and directories */
+const twit = require('twit') /* This is the library we are going to use to access the twitter API */
+let config = require('./config.js') /* We need to import our config.js, so we can initialize our "twit" module */
 
-//initailze our express app.
-const app = express();
 
-let tweets;
+/* Init our Twitter object with credentials we specified in Config.js */
+const Twitter = new twit(config)
 
-var getMlhacksTweets = function(){
-	var params = {
-		q: '@MLHacks',
-		result_type: 'recent',
-		lang: 'en'
+/*Init our express app */
+const app = express()
+
+/* Init our tweet variable */
+let tweets
+
+/* 
+	Here we will define our function that makes 
+	the request to the Twitter API 
+*/
+let getMlhacksTweets = function(){
+	/* 
+		You can make all sorts of requests to the Twitter API, so we have to 
+		specify what sort of request we want to make. 
+
+		For more information, see the Twitter API documetation:
+		https://developer.twitter.com/en/docs/api-reference-index.html
+
+		And the "twit" module documentation: 
+		https://www.npmjs.com/package/twit 
+	*/
+	let params = {
+		q: '@MLHacks', /* 'q:' specifies a query referencing '@MLHacks' */
+		result_type: 'recent', /* We want the most recent tweets */
+		lang: 'en' /* We'd like tweets in english */
 	}
 	
-	Twitter.get('search/tweets', params, function(err, data,response){
-		//if there are no errors
+
+	/*Lets make the 'search' request for 'tweets' to the twitter API */
+	Twitter.get('search/tweets', params, function(err, data){
+		
+		/*If there are no errors with our request...*/
 		if(!err){
-			//console.log(data);
-			console.log('Tweets Retrieved')
-			tweets = data
-			globalTweets = data;
+			console.log('Tweets Retrieved') 
+			tweets = data /* Assign our local tweets variable the new data */
 		}
-		//if unable to Search a tweet
+		/*If we have an error, then just write to console */
 		else {
-			console.log('Something went wrong while searching...');
+			console.log('Something went wrong while searching...')
 		}
 	});
-
 }
 
+/* 
+	We need to set up our server and define how the 
+	frontend will get the the UI and the tweets from 
+	the server.
+*/ 
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname,'client/build')));
-
+/* 
+	We need to tell the server where to find the files for the UI. In this case, we
+	pointed them towards the 'client/build' directory 
+*/
+app.use(express.static(path.join(__dirname,'client/build')))
 app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000', 'https://salty-ridge-74365.herokuapp.com/');
+    /*
+    	These are the websites that we will allow to make requests from our server. 
+		In this case, we want to allow or local development domain (http://localhost:3000)
+		and our production domain (https://salty-ridge-74...)
+    */
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000', 'https://salty-ridge-74365.herokuapp.com/')
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    /* These are the requests we will allow. In this case, we only need to allow GET requests. */
+    res.setHeader('Access-Control-Allow-Methods', 'GET')
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
+    /* Pass to next layer of middleware */
     next();
 });
 
-// Put all API endpoinds under '/api'
+/*Define our API as "/api/tweets". This is where our frontend will make requests.*/
 app.get('/api/tweets',(req,res) => {
-	console.log("GET:API")
-	res.json(tweets)
+	console.log("GET:API") /* Write to console each time a GET request has been recieved */
+	res.json(tweets)		/* Send the tweets data as a response */ 
 });
 
-//The 'catch all' handler: for any request that doesn't match the one above,
-// send back React's index.html file.
-
+/*
+	The 'catch all' handler: for any request that doesn't match the one above,
+	send back React's index.html file.
+*/
 app.get('*', (req,res) => {
 	console.log("GET: Catchall")
 	res.sendFile(path.join(__dirname+'/client/build/index.html'))
 });
 
+/*
+	Finally, we need to start our backend server.
+	We define the port we want the server to listen on and then start the sever.
+*/
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-	//init our tweet object when the server starts
+	/*
+		First, we want to initiailze our tweet data when the server starts.
+		If we don't do this, we will have to wait 5 minutes before we will have
+		any useful data.
+	*/
 	getMlhacksTweets()
 	
-	//set our tweet object to update every 5 minutes.
+	/*
+		Next, set our tweet data to update every 5 minutes (60000 milliseconds)
+	*/
 	setInterval(getMlhacksTweets,60000)
-	console.log(`Server running on port {port}`)
+	console.log(`Server running on port ` + port) /* ... and log that the server is actually running! */
+
 });
